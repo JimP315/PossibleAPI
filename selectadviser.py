df17d60f0f6724db1941edbb145dbfb0f7f2c0a5
Jim <jimp3105@gmail.com>
2022-07-18 22:08:57 +1000

    python source file

---
+++ selectadviser.py
@@ -0,0 +1,117 @@
+import streamlit as st
+#import pickle
+import pandas as pd
+
+
+
+# all advisers to select from
+all_advisers = [
+    "Jim Penner - Adviser Ratings 4.95", 
+    "Jon Morrow - Adviser Ratings 4.8",
+    "Blaine Miller - Adviser Ratings 4.75",
+    "Maria Anderson - Adviser Ratings 4.7",  
+]
+
+# all services to choose from 
+services = [
+    "Investment only",
+    "Life Insurance Review/Advice",
+    "Super Investment / Rollover Advice",
+    "Super contribution Advice",
+    "Lending/Mortgage/Margin Lending Advice",
+    "Estate Planning Advice",
+    "Retirement Planning Advice",
+    "Centrelink and/or Aged Care Advice",
+    "3 - 4 of the Above services",
+    "Comprehensive Advice, ie all of the above",
+  
+]
+
+# risk profile that suits the potential client 
+risk_profile = [
+    "Conservative",
+    "Moderate", 
+    "Balanced", 
+    "Balanced Growth", 
+    "Growth",
+ 
+]
+
+# what will we try to predict here....maybe end value based on risk profile 
+def predict_outcome(model, data):
+    # build dict of data to predict from
+    dict_data = {
+        "services": data[0],
+        "risk profile": data[1],
+        "investment_aud": data[2],
+        "investment_regular": data[3],
+        "investment_timeframe": data[4],
+        "investment_goal": data[5],
+    }
+
+    # build dataframe and transpose to ensure data is in the right structure
+    data = pd.DataFrame.from_dict(dict_data, orient="index").T
+
+    # return prediction
+    return model.predict(data)
+
+
+def main():
+    st.title("Get started on your journey to Financial Success!")
+
+    st.write('Let us know a bit of how we can help you as well as your appetite for risk.')
+
+    current_adviser = st.sidebar.selectbox("Which adviser would you like to work with to achieve financial success?", all_advisers)
+
+    # get user input for inputs to the model prediction
+    services = st.sidebar.selectbox("How are we able to assist you", services)
+    risk_appetite = st.sidebar.selectbox("Risk Profile", risk_profile)
+    investment_aud = st.sidebar.text_input("Initial Investment Amount ($AUD)", value=0)
+    investment_timeframe = st.sidebar.text_input(
+        "How long will you likely invest for (years)", value=0
+    )
+    investment_regular = st.sidebar.text_input(
+        "How much are you willing to invest on a monthly basis in order to achieve your goal? ($AUD)", value=0
+    )
+    investment_goal = st.sidebar.text_input(
+        "What is your end goal in terms of expected capital value target? ($AUD)", value=0
+    )
+
+    # create button to generate prediction - this may be more along the lines of the expected rate of return needed to achieve goal or the likely value in the expected timeframe. 
+    if st.button("Predict the outcome!"):
+        prediction = predict_outcome(
+            all_models[current_adviser],
+            [services, risk_appetite, investment_aud, investment_timeframe, investment_regular, investment_goal,],
+        )
+        if prediction[0] == "successful":
+            st.write("You are likely to achieve your financial goal!")
+        else:
+            st.write("You may have a funding shortfall :( ")
+
+    # short section describing each model
+    st.markdown("## More about our University qualified Advisers")
+
+    if current_adviser == "Jim Penner - Adviser Ratings 4.95":
+        st.markdown(
+            "### Jim's background:  Bachelor of Commerce (Hon), University of Manitoba, 1992; Diploma of Financial Planning; Accredited Aged Care Specialist, 2016; Stanford Executive Program; Self Managed Superannuation Accreditation; ASX Listed Products Accreditation, Monash FinTech Bootcamp", 
+        )
+        st.image('images\Jim.jpg')
+    elif current_adviser == "Jon Morrow - Adviser Ratings 4.8":
+        st.markdown(
+            "### Jon's background: Bachelor of Business, Queensland University of Technology, Australia, 2000; Advanced Diploma of Financial Planning ; Diploma of Financial Services (Financial Planning); Self Managed Superannuation Accreditation; ASX Listed Products Accreditation; Diploma of Financial Services (Finance/Mortgage Broking)"
+        )
+        st.image('images\p3fplogo.jpg"')
+    elif current_adviser == "Blaine Miller - Adviser Ratings 4.75":
+        st.markdown(
+            "### Blaine's background: Bachelor of Business; Advanced Diploma of Financial Planning, 2020; Accredited Aged Care Specialist, 2019; Diploma of Financial Planning; ETF Certification."
+        )
+        st.image('images/Blaine.jpg')
+    else: 
+        st.markdown(
+            "### Maria's background:  Accredited Aged Care Specialist, 2020; Advanced Diploma of Financial Planning; Diploma of Financial Planning; Master of Science Business Analysis & Finance; Bachelor of Arts Business Economics"
+        )
+        st.image('images/Maria.jpg')
+
+
+if __name__ == "__main__":
+    main()
